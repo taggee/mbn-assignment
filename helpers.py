5c98b7d07e5fdcbd4683219913b1da5abfc9061f
ajokinen07 <antti.2.jokinen@aalto.fi>
2021-02-26 17:45:40 +0200

    Minor updates on helpers and models, made result matrices look nicer using data frames

--- helpers.py
+++ helpers.py
@@ -4,6 +4,15 @@ import csv
 import matplotlib.pyplot as plt
 
 
+def result_to_df(array: np.array):
+    """Make array of edges into a neat data frame."""
+    genes = ['ASH1', 'CBF1', 'GAL4', 'GAL80', 'SWI5']
+    df = pd.DataFrame(array.astype(int))
+    df.columns=genes
+    df.index=genes
+    return df
+
+
 def read_from_csv(fname: str):
     """Get data df with sorted columns."""
     df = pd.read_csv(fname, sep=';', index_col=0)
@@ -11,9 +20,12 @@ def read_from_csv(fname: str):
     return df
 
 
-def plot_array(array: np.array):
+def plot_result(df: pd.DataFrame):
     """Plot the 5x5 gene dependency matrix."""
     fig = plt.figure(figsize=(5, 5))
-    plt.matshow(array, fignum=fig.number)
+    plt.matshow(df.values, fignum=fig.number)
     plt.title('Dependency Matrix', fontsize=12)
-    return fig
+    genes = ['ASH1', 'CBF1', 'GAL4', 'GAL80', 'SWI5']
+    plt.xticks(ticks=np.arange(0,5), labels=genes)
+    plt.yticks(ticks=np.arange(0,5), labels=genes)
+    return
--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+from helpers import result_to_df
 
 
-def correlation_model(df, threshold):
+def correlation_model(df: pd.DataFrame, threshold: float):
     corr_matrix = df.corr().values
     result = np.zeros((5, 5))
     for i in range(5):
         for j in range(5):
             if corr_matrix[i, j] >= threshold:
                 result[i, j] = 1
-    return pd.DataFrame(result)
+    return result_to_df(result)
